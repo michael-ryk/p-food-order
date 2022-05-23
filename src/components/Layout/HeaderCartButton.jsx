25327ce0b71bb00598799cdb880d7c0578395690
MichaelR <spider747@gmail.com>
2022-05-23 20:39:36 +0300

    Add Cart icon

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,10 +1,13 @@
 import classes from './HeaderCartButton.module.css';
+import cart from '../../assets/cart.png';
 
 const HeaderCartButton = props => {
 
   return (
     <button className={classes.button}>
-      <span className={classes.icon}></span>
+      <span className={classes.icon}>
+        <img src={cart} alt='cart'/>
+      </span>
       <span>Cart</span>
       <span className={classes.badge}>5</span>
     </button>
