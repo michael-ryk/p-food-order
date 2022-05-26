f90abc327c32401b8a91802913233f1976e274ea
MichaelR <spider747@gmail.com>
2022-05-26 21:54:37 +0300

    Add order quantity form with button

--- src/components/Food/FoodItem/FoodItemForm.jsx
+++ src/components/Food/FoodItem/FoodItemForm.jsx
@@ -1,10 +1,18 @@
 import classes from './FoodItemForm.module.css';
+import Input from '../../UI/Input';
 
 const FoodItemForm = props => {
 
 return (
   <form className={classes.form}>
-    <input />
+    <Input label='Amount' input={{
+      id: 'amount',
+      type: 'number',
+      min: '1',
+      max: '5',
+      step: '1',
+      defaultValue: '1'
+    }}/>
     <button>Add</button>
   </form>
 );
