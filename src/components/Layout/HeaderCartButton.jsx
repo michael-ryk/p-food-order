c8f1d3aafcacb8b02e77b080884f0dcb4e7b0b15
MichaelR <spider747@gmail.com>
2022-05-29 22:19:42 +0300

    use number of items in cart badge from context

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,18 +1,22 @@
+import { useContext } from 'react';
+
 import classes from './HeaderCartButton.module.css';
 import cart from '../../assets/cart.png';
+import ItemsContext from '../store/ItemsContext';
 
-const HeaderCartButton = props => {
+const HeaderCartButton = (props) => {
+  const itemsFromContext = useContext(ItemsContext);
+  const numberOfCartItems = itemsFromContext.items.length;
 
   return (
     <button className={classes.button} onClick={props.onClickSubmit}>
       <span className={classes.icon}>
-        <img src={cart} alt='cart'/>
+        <img src={cart} alt='cart' />
       </span>
       <span>Cart</span>
-      <span className={classes.badge}>5</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
-  )
-
-}
+  );
+};
 
 export default HeaderCartButton;
