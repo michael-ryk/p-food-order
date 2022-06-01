dcbb81f1f54f6cd3f65be901790a2e164f64d185
MichaelR <spider747@gmail.com>
2022-06-01 22:26:21 +0300

    Group items with same name and sum quantity

--- src/components/store/ItemsProvider.jsx
+++ src/components/store/ItemsProvider.jsx
@@ -4,16 +4,46 @@ import ItemsContext from './ItemsContext';
 // Reducer default value
 const cartDefault = {
   items: [],
-  totalCartPrice: 0
-}
+  totalCartPrice: 0,
+};
 
 // Reducer actions - returns new state of cartState
 const cartReducer = (state, action) => {
   if (action.action === 'ADD') {
-    const totalOrderedItems = state.items.concat(action.item);
-    const totalOrderedPrice = state.totalCartPrice + action.item.price * action.item.amount;
-    
+    // Check if this items already order and add to existing item
+
+    const existingItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+    const existingItem = state.items[existingItemIndex];
+    let updatedItem;
+    let updatedListOfItems;
+
     // Debug log
+    // console.log('!!! current items !!!');
+    // console.log(state.items);
+    // console.log('User input item:');
+    // console.log(action.item);
+
+    if (existingItem) {
+      updatedItem = {
+        ...existingItem,
+        amount: existingItem.amount + action.item.amount,
+      };
+      updatedListOfItems = [...state.items];
+      updatedListOfItems[existingItemIndex] = updatedItem;
+    } else {
+      updatedItem = {...action.item};
+      updatedListOfItems = state.items.concat(updatedItem);
+    }
+
+    const totalOrderedItems = updatedListOfItems;
+    const totalOrderedPrice =
+      state.totalCartPrice + action.item.price * action.item.amount;
+      
+    // Debug log
+    // console.log('totalOrderedItems:');
+    // console.log(totalOrderedItems);
     // console.log("=== ItemsProvider - cartReducer func === ");
     // console.log('// State')
     // console.log(state);
@@ -22,17 +52,16 @@ const cartReducer = (state, action) => {
     // console.log('// Calculations of total items and price')
     // console.log(totalOrderedItems)
     // console.log(totalOrderedPrice)
-    
+
     return {
       items: totalOrderedItems,
-      totalCartPrice: totalOrderedPrice
-    }
+      totalCartPrice: totalOrderedPrice,
+    };
   }
   return cartDefault;
-}
+};
 
 const ItemsProvider = (props) => {
-
   // Array of ordered items + total price calculated
   const [cartState, dispatchCart] = useReducer(cartReducer, cartDefault);
 
@@ -42,11 +71,11 @@ const ItemsProvider = (props) => {
   // console.log(cartState);
 
   const addItemToCart = (item) => {
-    dispatchCart({action: 'ADD', item: item});
+    dispatchCart({ action: 'ADD', item: item });
   };
 
   const removeItemFromCart = (id) => {
-    dispatchCart({action: 'REMOVE', id: id});
+    dispatchCart({ action: 'REMOVE', id: id });
   };
 
   const itemsContext = {
