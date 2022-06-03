6c1c23ddda04ef4a76d94fda546a6636e5e594e4
MichaelR <spider747@gmail.com>
2022-06-03 20:36:13 +0300

    Add bounce effect on cart button when count changes

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 
 import classes from './HeaderCartButton.module.css';
 import cart from '../../assets/cart.png';
@@ -6,12 +6,35 @@ import ItemsContext from '../store/ItemsContext';
 
 const HeaderCartButton = (props) => {
   const itemsFromContext = useContext(ItemsContext);
-  const numberOfCartItems = itemsFromContext.items.reduce((totalAmount ,item) => {
-    return totalAmount + item.amount;
-  }, 0);
+  const numberOfCartItems = itemsFromContext.items.reduce(
+    (totalAmount, item) => {
+      return totalAmount + item.amount;
+    },
+    0
+  );
+
+  // Bounce effect on button when user add new item
+  const [animationAttached, setAnimationAttached] = useState(false);
+  useEffect(() => {
+    if (itemsFromContext.items.length === 0) return;
+
+    setAnimationAttached(true);
+
+    //Remove class after animation finished
+    const timer = setTimeout(() => {
+      setAnimationAttached(false);
+    }, 100);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [itemsFromContext.items]);
 
   return (
-    <button className={classes.button} onClick={props.onClickSubmit}>
+    <button
+      className={`${classes.button} ${animationAttached && classes.bounce}`}
+      onClick={props.onClickSubmit}
+    >
       <span className={classes.icon}>
         <img src={cart} alt='cart' />
       </span>
