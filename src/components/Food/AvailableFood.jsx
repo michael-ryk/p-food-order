6801991bff1c914ddb126f15fa1908c57363b2d6
MichaelR <spider747@gmail.com>
2022-06-30 20:21:00 +0300

    Fetch Data from DB on button press

--- src/components/Food/AvailableFood.jsx
+++ src/components/Food/AvailableFood.jsx
@@ -1,11 +1,29 @@
 import DUMMY_FOOD from './DummyFoodList';
+import {useEffect} from 'react';
 import classes from './AvailableFood.module.css'
 import Card from '../UI/Card';
 import FoodItem from './FoodItem/FoodItem';
 
 const AvailableFood = () => {
+
+  // Fetch data from DB once when page loaded
+  // useEffect(()=>{
+
+  // },[]);
+
+  async function fetchFoodList () {
+    console.log('Start Async Function to fetch data')
+    const response = await fetch("https://p-food-order-default-rtdb.firebaseio.com/meals.json");
+    const data = await response.json();
+    
+    console.log(data);
+  }
+
+
+
   return (
     <section className={classes.food}>
+      <button onClick={fetchFoodList}>Fetch List</button>
       <Card>
         <ul>
           {DUMMY_FOOD.map(item => {
