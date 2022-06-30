48ca259d80c740f6c66e8486897719f6ba48a069
MichaelR <spider747@gmail.com>
2022-06-30 21:46:07 +0300

    Handle request errors

--- src/components/Food/AvailableFood.jsx
+++ src/components/Food/AvailableFood.jsx
@@ -1,22 +1,28 @@
-import {useState, useEffect} from 'react';
-import classes from './AvailableFood.module.css'
+import { useState, useEffect } from 'react';
+import classes from './AvailableFood.module.css';
 import Card from '../UI/Card';
 import FoodItem from './FoodItem/FoodItem';
 
 const AvailableFood = () => {
-
   const [foodList, setFoodList] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
 
   // Fetch data from DB once when page loaded
-  useEffect(()=>{
-
+  useEffect(() => {
     // Trick to use expression and call it to make it work from useEffect
     const fetchFoodList = async () => {
       setIsLoading(true);
-      const response = await fetch("https://p-food-order-default-rtdb.firebaseio.com/meals.json");
+      const response = await fetch(
+        'https://p-food-order-default-rtdb.firebaseio.com/meals.json'
+      );
+
+      if (!response.ok) {
+        throw new Error('Problem connect to DB!');
+      }
+
       const data = await response.json();
-      
+
       // Rearange Food list to acceptable form used in code
       const foodFromDb = [];
       for (const key in data) {
@@ -30,22 +36,42 @@ const AvailableFood = () => {
 
       setFoodList(foodFromDb);
       setIsLoading(false);
-    }
-    fetchFoodList();
-  },[]);
+    };
+
+    // Handle error by catch promise in async function
+    fetchFoodList().catch((err) => {
+      setIsLoading(false);
+      setError(err.message);
+    });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section className={classes.isLoading}>
+        <p>Data Loading ...</p>
+      </section>
+    )
+  }
+
+  if (error) {
+    return (
+      <section className={classes.error}>
+        <p>{error}</p>
+      </section>
+    );
+  }
 
   return (
     <section className={classes.food}>
-      {isLoading && <p className={classes.isLoading}>Data Loading ...</p>}
       <Card>
         <ul>
-          {foodList.map(item => {
-            return <FoodItem key={item.id} {...item}/>})
-            }
+          {foodList.map((item) => {
+            return <FoodItem key={item.id} {...item} />;
+          })}
         </ul>
       </Card>
     </section>
   );
-}
+};
 
 export default AvailableFood;
