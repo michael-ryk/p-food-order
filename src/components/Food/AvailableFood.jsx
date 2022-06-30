c35339e93f94c2284839ffec73b915df06c3c164
MichaelR <spider747@gmail.com>
2022-06-30 20:39:43 +0300

    Show fetched food list instead of dummy list

--- src/components/Food/AvailableFood.jsx
+++ src/components/Food/AvailableFood.jsx
@@ -1,19 +1,32 @@
-import DUMMY_FOOD from './DummyFoodList';
-import {useEffect} from 'react';
+import {useState, useEffect} from 'react';
 import classes from './AvailableFood.module.css'
 import Card from '../UI/Card';
 import FoodItem from './FoodItem/FoodItem';
 
 const AvailableFood = () => {
 
+  const [foodList, setFoodList] = useState([]);
+
   // Fetch data from DB once when page loaded
   useEffect(()=>{
+
     // Trick to use expression and call it to make it work from useEffect
     const fetchFoodList = async () => {
-      console.log(':: Fetch Data from DB ::')
       const response = await fetch("https://p-food-order-default-rtdb.firebaseio.com/meals.json");
       const data = await response.json();
-      console.log(data); 
+      
+      // Rearange Food list to acceptable form used in code
+      const foodFromDb = [];
+      for (const key in data) {
+        foodFromDb.push({
+          id: key,
+          name: data[key].name,
+          description: data[key].description,
+          price: data[key].price,
+        });
+      }
+
+      setFoodList(foodFromDb);
     }
     fetchFoodList();
   },[]);
@@ -22,7 +35,7 @@ const AvailableFood = () => {
     <section className={classes.food}>
       <Card>
         <ul>
-          {DUMMY_FOOD.map(item => {
+          {foodList.map(item => {
             return <FoodItem key={item.id} {...item}/>})
             }
         </ul>
