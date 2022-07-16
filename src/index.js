18169675cda92fb04521a3f47f3ba9c0bd2ea4fb
MichaelR <spider747@gmail.com>
2022-07-16 18:25:54 +0300

    Wrap index.js with Provider

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
+import { Provider } from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
+import store from './components/store/index';
 import App from './App';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
